0a96ea64df60782d550924092835ec9063eb47bb
1975kir <km1975@yandex.ru>
2022-03-08 18:17:46 +0300

    Initial commit

---
+++ src/SumDemo.java
@@ -0,0 +1,10 @@
+public class SumDemo {
+            public static void main(String[] args) {
+            for (int i = 0; i < 15; i++) {
+                int sum = 0;
+                sum = sum + i;
+                System.out.println("Сумма: " + sum);
+            }
+        }
+    }
+
